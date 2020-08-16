d88f43a127ef77ac814ab315decaea47d96ed3d4
S. M. JAHANGIR <smjxpro@gmail.com>
2020-08-16 12:24:55 +0600

    display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 import { from } from 'rxjs';
 
 @Component({
@@ -8,10 +9,13 @@ import { from } from 'rxjs';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() {}
 
